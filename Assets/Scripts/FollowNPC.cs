b00f7dbd381ae936180a63e68b636e6bcbf563dd
Danny Ryan <dannyryan1980+github@gmail.com>
2020-07-19 21:54:12 +0100

    added hoz split screen

---
+++ Assets/Scripts/FollowNPC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowNPC : MonoBehaviour
+{
+    public GameObject nonplayercharacter;
+    private Vector3 offset = new Vector3(0, 5, 20); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = nonplayercharacter.transform.position + offset;
+    }
+}
