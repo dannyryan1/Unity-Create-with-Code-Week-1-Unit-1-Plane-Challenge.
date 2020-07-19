b426ef023fec019fcef13b24512e57a12d2dd05d
Danny Ryan <dannyryan1980+github@gmail.com>
2020-07-19 21:33:50 +0100

    completed medium bonus task
    Add a couple of other cars that are automatically driving down the road, which the player also has to avoid.

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private float turnSpeed = 45.0f;
+    private float forwardInput = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on the vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+    }
+}
